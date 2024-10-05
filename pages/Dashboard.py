fd4651a15e5dae42907e67a0d2aad946b9636cf5
Yosra Naser <153253079+Yosranaser@users.noreply.github.com>
2024-10-06 02:07:07 +0300

    Update Dashboard.py

--- pages/Dashboard.py
+++ pages/Dashboard.py
@@ -18,7 +18,7 @@ import pickle
 st.set_page_config(page_title="churn segmenation", page_icon="✈️", layout='wide')
 
 # Loading data
-uploaded_file = st.file_uploader("Choose a file")
+#uploaded_file = st.file_uploader("Choose a file")
 #if uploaded_file is not None:
     #df = pd.read_csv(uploaded_file)
 
